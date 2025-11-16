340ad43e5335d925f423af36b03b48a488659a9a
[GDGOMES] Gustavo da Silva Gomes <gustavo.sgomes@embraer.com.br>
2025-11-16 13:39:23 -0300

    style(user): formatting

--- src/user/domain/value-objects/uuid.vo.ts
+++ src/user/domain/value-objects/uuid.vo.ts
@@ -1,36 +1,35 @@
-
-import { v4 as uuid } from 'uuid'
+import { v4 as uuid } from "uuid";
 
 abstract class EntityId<T> {
-  protected readonly _value: string
+  protected readonly _value: string;
 
   constructor(value?: string) {
     if (value) {
       if (!this.isValidId(value)) {
-        throw new Error(`Invalid ${this.constructor.name} format: ${value}`)
+        throw new Error(`Invalid ${this.constructor.name} format: ${value}`);
       }
-      this._value = value
+      this._value = value;
     } else {
-      this._value = uuid()
+      this._value = uuid();
     }
   }
 
   get value(): string {
-    return this._value
+    return this._value;
   }
 
   equals(other: EntityId<T>): boolean {
-    return this._value === other._value
+    return this._value === other._value;
   }
 
   toString(): string {
-    return this._value
+    return this._value;
   }
 
   private isValidId(value: string): boolean {
     const uuidRegex =
-      /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
-    return uuidRegex.test(value)
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    return uuidRegex.test(value);
   }
 }
 
