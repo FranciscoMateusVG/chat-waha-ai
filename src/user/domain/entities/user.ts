dd69657b4e9e8a393d3be2028a84cada57fcf110
[GDGOMES] Gustavo da Silva Gomes <gustavo.sgomes@embraer.com.br>
2025-11-16 13:40:39 -0300

    fix(user): fix user errors

--- src/user/domain/entities/user.ts
+++ src/user/domain/entities/user.ts
@@ -1,44 +1,38 @@
-import { UserId } from "../value-objects/uuid.vo.ts"
-import { PhoneNumber } from "../value-objects/phone-number.vo.ts";
+import { UserId } from "../value-objects/uuid.vo";
+import { PhoneNumber } from "../value-objects/phone-number.vo";
 
 export class User {
-	private id: UserId;
-	private name: string;
-	private phone: PhoneNumber;
+  private _id: UserId;
+  private _name: string;
+  private _phone: PhoneNumber;
 
-	constructor({
-		private readonly id: UserId,
-		private readonly name: string,
-		private readonly phone: PhoneNumber 
-	}) {
-		this.id = id;
-		this.name = name;
-		this.phone = phone;
-	}
+  constructor(props: { id: UserId; name: string; phone: PhoneNumber }) {
+    this._id = props.id;
+    this._phone = props.phone;
+    this._name = props.name;
+  }
 
-	static create(
-		name: string,
-		phoneNumber: PhoneNumber,
-		id?: UserId
-	) {
-		return new User({
-			id: id ?? new UserId(),
-			name: name,
-			phoneNumber: phoneNumber
-		})
-	}
+  static create(name: string, phone: PhoneNumber, id?: UserId) {
+    return new User({
+      id: id ?? new UserId(),
+      name: name,
+      phone: phone,
+    });
+  }
 
+  equals(other: User) {
+    return this.id === other.id;
+  }
 
-	get id() {
-		return this.id;
-	}
+  get id() {
+    return this._id;
+  }
 
-	get name() {
-		return this.name;
-	}
+  get name() {
+    return this._name;
+  }
 
-	get phone() {
-		return this.phone;
-	}
+  get phone() {
+    return this._phone;
+  }
 }
-
