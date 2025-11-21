07d7396045eca8296269e0f58a37785fdcb6428b
[GDGOMES] Gustavo da Silva Gomes <gustavo.sgomes@embraer.com.br>
2025-11-20 21:03:28 -0300

    feat(user): create email value object

--- src/user/domain/entities/user.ts
+++ src/user/domain/entities/user.ts
@@ -1,22 +1,31 @@
 import { UserId } from "../value-objects/uuid.vo";
 import { PhoneNumber } from "../value-objects/phone-number.vo";
+import { Email } from "../value-objects/email.vo";
 
 export class User {
   private _id: UserId;
   private _name: string;
   private _phone: PhoneNumber;
+  private _email: Email;
 
-  constructor(props: { id: UserId; name: string; phone: PhoneNumber }) {
+  constructor(props: {
+    id: UserId;
+    name: string;
+    phone: PhoneNumber;
+    email: Email;
+  }) {
     this._id = props.id;
     this._phone = props.phone;
     this._name = props.name;
+    this._email = props.email;
   }
 
-  static create(name: string, phone: PhoneNumber, id?: UserId) {
+  static create(name: string, phone: PhoneNumber, email: Email, id?: UserId) {
     return new User({
       id: id ?? new UserId(),
       name: name,
       phone: phone,
+      email: email,
     });
   }
 
