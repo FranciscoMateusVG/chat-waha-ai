523efbe8b6a6da101b1d54ef54e7b7be6d12b966
diegochagas30 <140138148+diegochagas30@users.noreply.github.com>
2025-11-25 09:09:53 -0300

    feat: integração do frontend com a api

--- api/src/main.ts
+++ api/src/main.ts
@@ -10,7 +10,6 @@ dotenv.config()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true })
 
-  // Enable CORS for frontend integration
   app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { api } from './services/api'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [response, setResponse] = useState('')
+
+  const getSystemPrompt = async () => {
+    try {
+      const response = await api.get('knowledge/system-prompt')
+      if (response.data.success && response.data.data) {
+        setResponse(response.data.data.content || 'Não há nenhum system prompt.')
+      }
+    } catch (error) {
+      console.error(error)
+      setResponse('Erro ao conectar com a API')
+    }
+  }
+
+  useEffect(() => {
+    getSystemPrompt()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>API Response:</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          {response}
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
